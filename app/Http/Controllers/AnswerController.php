4d98acb6fc1eeab35f8ff29323addb0dc2584314
markaz mis <mis.it.fava@gmail.com>
2024-06-10 13:55:47 +0330

    change

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -18,7 +18,7 @@ class AnswerController extends Controller
 {
     public function index()
     {
-        $userAnswer = UserAnswer::skip(2)->get();
+        $userAnswer = UserAnswer::offset(2)->get();
         dd($userAnswer->nthElement(3)->answer);
         return view('admin.answer.index', compact('userAnswer'));
     }
